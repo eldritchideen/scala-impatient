a5044a06547d767e9ca028e848e4d2119c386d92
Steven Cook <steve.j.cook@gmail.com>
2013-12-19 04:50:54 +1000

    A couple more chapter 5 exercises done.

--- src/main/scala/com/eldritchideen/exercises/Chapter5.scala
+++ src/main/scala/com/eldritchideen/exercises/Chapter5.scala
@@ -1,5 +1,7 @@
 package com.eldritchideen.exercises
 
+import scala.beans.BeanProperty
+
 /**
  * Created by sjc on 8/12/13.
  */
@@ -11,3 +13,41 @@ class Time(val hours: Int, val minutes: Int) {
     this.hours < other.hours || (this.hours == other.hours && this.minutes < other.minutes)
   }
 }
+
+// Exercise 4
+class Time2(private val _hours: Int, private val _minutes: Int) {
+  private val minPastMidnight = _hours * 60 + _minutes
+
+  def hours = minPastMidnight / 60
+
+  def minutes = minPastMidnight % 60
+
+  def before(other: Time2) = {
+    this.minPastMidnight < other.minPastMidnight
+  }
+}
+
+// Exercise 5
+class Student(@BeanProperty var name:String, @BeanProperty var id:Int) {
+
+}
+
+/*
+Result of decompiling the Student class
+
+>  javap Student.class
+
+Compiled from "Chapter5.scala"
+public class com.eldritchideen.exercises.Student {
+public java.lang.String name();
+public void name_$eq(java.lang.String);
+public void setName(java.lang.String);
+public int id();
+public void id_$eq(int);
+public void setId(int);
+public java.lang.String getName();
+public int getId();
+public com.eldritchideen.exercises.Student(java.lang.String, int);
+}
+
+*/
--- src/test/scala/com/eldritchideen/exercises/Chapter5Test.scala
+++ src/test/scala/com/eldritchideen/exercises/Chapter5Test.scala
@@ -18,4 +18,21 @@ class Chapter5Test extends FunSuite {
     assert(t4.before(t1))
   }
 
+  test("Time2") {
+    val t1 = new Time2(10, 30)
+    val t2 = new Time2(6, 30)
+    val t3 = new Time2(10, 35)
+    val t4 = new Time2(10,19)
+
+    assert(t1.before(t3))
+    assert(!t1.before(t2))
+    assert(t4.before(t1))
+  }
+
+  test("Java Beans") {
+    // Test being able to call java beans setters and getters.
+    val s = new Student("Steve", 12345)
+    s.setId(6789)
+    assert(s.getId() == 6789)
+  }
 }
