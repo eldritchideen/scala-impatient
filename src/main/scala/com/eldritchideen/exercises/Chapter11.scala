660c8e0a504d4bea22d52514880b2c4882953539
Steven Cook <steve.j.cook@gmail.com>
2014-01-04 13:56:56 +1000

    Added less than operator and cleaned up tests.

--- src/main/scala/com/eldritchideen/exercises/Chapter11.scala
+++ src/main/scala/com/eldritchideen/exercises/Chapter11.scala
@@ -7,6 +7,13 @@ class Chapter11 {
 
 }
 
+/**
+ * Simple class representing Monetary amounts in dollars and cents.
+ * Exercise 4, implement infix and comparison operators.
+ *
+ * @param dollars
+ * @param cents
+ */
 class Money(val dollars: Int, val cents: Int) {
 
   def +(other: Money) = {
@@ -21,6 +28,8 @@ class Money(val dollars: Int, val cents: Int) {
     else
       new Money(amount / 100, amount % 100)
   }
+
+  def <(other: Money) = totalCents(this) < totalCents(other)
 
   override def equals(other: Any) = other match {
     case that: Money => this.dollars == that.dollars && this.cents == that.cents
--- src/test/scala/com/eldritchideen/exercises/Chapter11Test.scala
+++ src/test/scala/com/eldritchideen/exercises/Chapter11Test.scala
@@ -5,32 +5,38 @@ import org.scalatest.{Matchers, FunSuite}
 /**
  * Created by sjc on 3/01/14.
  */
-class Chapter11Test extends FunSuite with Matchers {
+
+// Test data
+trait Monies {
+  val m0 = new Money(0,0)
+  val m1 = new Money(1,50)
+  val m2 = new Money(0,75)
+  val m3 = new Money(3,0)
+}
+
+class Chapter11Test extends FunSuite with Matchers with Monies {
+
+
 
   test("creation of Money") {
-    val m1 = new Money(0,0)
-    val m2 = new Money(1,50)
-
-    assert(m1.dollars === 0)
-    assert(m1.cents === 0)
-    assert(m2.dollars === 1)
-    assert(m2.cents === 50)
+    assert(m0.dollars === 0)
+    assert(m0.cents === 0)
+    assert(m1.dollars === 1)
+    assert(m1.cents === 50)
   }
 
   test("Adding money together") {
-    val m1 = new Money(1,50)
-    val m2 = new Money(0,75)
-
     (m1 + m2) should equal(new Money(2,25))
   }
 
   test("Subtracting money") {
-    val m1 = new Money(1,50)
-    val m2 = new Money(0,75)
-    val m3 = new Money(3,0)
-
     (m1 - m2) should equal(new Money(0,75))
     (m1 - m3) should equal(new Money(0,0))
+  }
+
+  test("Comparison of monetary amounts") {
+    (m2 < m1) should equal(true)
+    (m3 < m1) should equal(false)
   }
 
 }
